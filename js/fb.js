129deee9fe162531b307699a0c53fe0b4a093967
Leon-Mac <leonardoli525@gmail.com>
2016-10-01 13:57:22 -0400

    fix fb login bug

--- js/fb.js
+++ js/fb.js
@@ -9,6 +9,7 @@
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
+      document.getElementById('fblog').style.display='none'
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
